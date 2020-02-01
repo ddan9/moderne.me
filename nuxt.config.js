2761d5ca6e213c69411e83ea65672bb33297381b
Иван Винокуров <vinokurov@corsoagency.info>
2020-02-01 17:25:38 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,11 @@ export default {
       {name: "msapplication-TileColor", content: "#f3eae5"},
       {name: "theme-color", content: "#ffffff"},
       {hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
-      {name: 'og:url', content: 'https://moderne.st'},
-      {name: 'og:type', content: 'website'},
-      {name: 'og:title', content: 'Moderne: Unleash the full potential of your creative team'},
-      {name: 'og:description', content: 'Moderne provides tons of highly tailored creative ad ideas and insights and offers a digital workspace to make brainstorms more effective for creative minds'},
-      {name: 'og:image', content: 'https://moderne.st/og-image-moderne.png'}
+      {name: 'og:url', property: 'https://moderne.st'},
+      {name: 'og:type', property: 'website'},
+      {name: 'og:title', property: 'Moderne: Unleash the full potential of your creative team'},
+      {name: 'og:description', property: 'Moderne provides tons of highly tailored creative ad ideas and insights and offers a digital workspace to make brainstorms more effective for creative minds'},
+      {name: 'og:image', property: 'https://moderne.st/og-image-moderne.png'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
