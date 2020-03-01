8a6f94843e410b960722abb3ad5918528bbbc9e1
Иван Винокуров <vinokurov@corsoagency.info>
2020-03-01 05:20:05 +0300

    Image elements do not have [alt] attributes

--- components/home/cards/cards.vue
+++ components/home/cards/cards.vue
@@ -4,9 +4,9 @@
     .cards__wrapper
       .cards__item(v-for="(item, index) in cards" :key="index" :class="`cards__item--${item.color}`")
         .cards__item-label {{item.label}}
-        img.cards__item-illustration--3(v-if="index === 0" src="@/assets/images/illustrations/cards-3.png" srcset="@/assets/images/illustrations/cards-3@2x.png 2x")
-        img.cards__item-illustration--1(v-if="index === 1" src="@/assets/images/illustrations/cards-1.png" srcset="@/assets/images/illustrations/cards-1@2x.png 2x")
-        img.cards__item-illustration--2(v-if="index === 2" src="@/assets/images/illustrations/cards-2.png" srcset="@/assets/images/illustrations/cards-2@2x.png 2x")
+        img.cards__item-illustration--3(:alt="item.title" v-if="index === 0" src="@/assets/images/illustrations/cards-3.png" srcset="@/assets/images/illustrations/cards-3@2x.png 2x")
+        img.cards__item-illustration--1(:alt="item.title" v-if="index === 1" src="@/assets/images/illustrations/cards-1.png" srcset="@/assets/images/illustrations/cards-1@2x.png 2x")
+        img.cards__item-illustration--2(:alt="item.title" v-if="index === 2" src="@/assets/images/illustrations/cards-2.png" srcset="@/assets/images/illustrations/cards-2@2x.png 2x")
         //.cards__item-illustration
         .cards__item-text
           span {{item.title}}
--- components/home/twoCols/twoCols.vue
+++ components/home/twoCols/twoCols.vue
@@ -4,9 +4,9 @@
     .twoCols__wrapper
       .twoCols__item.twoCols__item--1
         .twoCols__item-title Collaborate on great ideas with your team.
-        img.twoCols__item-img--big(src="@/assets/images/illustrations/twocols-1.png" srcset="@/assets/images/illustrations/twocols-1@2x.png 2x")
+        img.twoCols__item-img--big(alt="Collaborate on great ideas with your team." src="@/assets/images/illustrations/twocols-1.png" srcset="@/assets/images/illustrations/twocols-1@2x.png 2x")
       .twoCols__item.twoCols__item--2
-        img.twoCols__item-img--small(src="@/assets/images/illustrations/twocols-2.png" srcset="@/assets/images/illustrations/twocols-2@2x.png 2x")
+        img.twoCols__item-img--small(alt="Big ideas come from the unconscious." src="@/assets/images/illustrations/twocols-2.png" srcset="@/assets/images/illustrations/twocols-2@2x.png 2x")
         .twoCols__item-text “Big ideas come from the unconscious. But your unconscious has to be well informed, or your idea will be irrelevant. Stuff your conscious mind with information, then unhook your rational thought process”.
 </template>
 
