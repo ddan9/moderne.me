9b0d80fe5aef61ed226426539d7dd59f769d4c23
Иван Винокуров <vinokurov@corsoagency.info>
2020-03-01 05:36:06 +0300

    Form elements do not have associated labels

--- components/home/cta/cta.vue
+++ components/home/cta/cta.vue
@@ -5,7 +5,7 @@
       .cta__title We prepare the path to&nbsp;support your creative&nbsp;journey
       //form.cta__form#mc-embedded-subscribe-form.validate(action="http://moderne.us3.list-manage1.com/subscribe/post-json?u=82e7a91b930da0b541f0ae17d&id=5efba2eadb&c=?" method="get" name='mc-embedded-subscribe-form' target='_blank' novalidate='')
       form.cta__form#mc-embedded-subscribe-form.validate(action='https://moderne.us3.list-manage.com/subscribe/post?u=82e7a91b930da0b541f0ae17d&id=5efba2eadb' method='post' name='mc-embedded-subscribe-form' target='_blank' novalidate='')
-        label.cta__form-label Enter your email to get early access to Moderne
+        label.cta__form-label(for="mce-EMAIL") Enter your email to get early access to Moderne
         .cta__form-group
           input#mce-EMAIL.required.email.cta__form-input(type='email' name='EMAIL' placeholder='Your business email' required)
           div(style='position: absolute; left: -5000px;' aria-hidden='true')
