c1a6812d5b6d1300eec64cad89f2971ec322df5a
Иван Винокуров <vinokurov@corsoagency.info>
2020-03-05 05:19:00 +0300

    wip

---
+++ components/solutions/solutions-list/solutions-list.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+.solutions__list
+  .solutions__list-wrapper
+    nuxt-link.solutions__list-item(
+        v-for="(item, index) in solutions"
+        :to="item.url"
+      )
+      .solutions__list-item-title(
+        :style="`color: ${item.color}`"
+      ) {{ item.title }}
+      .solutions__list-item-lead {{ item.lead }}
+</template>
+
+<script>
+export default {
+  props: {
+  },
+  data () {
+    return {
+      solutions: [
+        {
+          title: 'Moderne for Agencies',
+          lead: 'Unleash the full potential of your creative team!',
+          color: '#FF9C5F',
+          url: '/solutions/for-agencies'
+        },
+        {
+          title: 'Moderne for Brands',
+          lead: 'Find the "A-ha!" moment quickly',
+          color: '#6D63D4',
+          url: '/solutions/for-brands'
+        },
+        {
+          title: 'Moderne for Startups',
+          lead: 'Stand out from your competitors with creative approach',
+          color: '#269D9B',
+          url: '/solutions/for-startups'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "solutions-list";
+</style>
---
+++ pages/solutions/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .solutions
+    .container
+      Solutions-list
+</template>
+
+<script>
+// import hero from '~/components/ui/hero/hero'
+// import plans from '~/components/pricing/pricing-plans/pricing-plans'
+
+import SolutionsList from '../../components/solutions/solutions-list/solutions-list'
+
+export default {
+  components: {
+    SolutionsList
+    // 'section-hero': hero,
+    // 'section-plans': plans
+  },
+  head: {
+    title: 'Solutions'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.solutions {
+  padding: 155px 0 0;
+  margin: 0 0 -105px;
+
+  @include mobile {
+    padding: vmin(100) 0 0;
+    margin: initial;
+  }
+}
+</style>
